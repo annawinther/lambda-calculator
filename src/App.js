7c9b3a56454b0f943d364a5c86598e58c819b6a7
Anna W <anski.anna@gmail.com>
2019-09-13 10:37:20 +0100

    classmane on app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
+import Operators from "./components/ButtonComponents/OperatorButtons/Operators";
+import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
 import Display from './components/DisplayComponents/Display';
 import "./App.css";
 // STEP 4 - import the button and display components
@@ -21,7 +23,15 @@ function App() {
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
         <Display />
-        <Numbers />
+        <div className="buttonContainer">
+          <div className="numbersSpecial">
+            <Specials />
+            <Numbers />
+          </div>
+          <div className="operatorContainer">
+            <Operators />
+          </div>
+        </div>
       </div>
     </div>
   );
